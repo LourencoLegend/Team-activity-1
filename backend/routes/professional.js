a3e95f3ba8f2b2f51cebaa5d072afd025e1822ec
LourencoLegend <lourencolegend@yahoo.com>
2023-11-11 15:09:23 -0700

    file name corrected

--- backend/routes/professional.js
+++ backend/routes/professional.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const professionalController = require('../controllers/professional');
+const professionalsController = require('../controllers/professional');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/', professionalController.getData);
+router.get('/', professionalsController.getData);
 // localhost:8080/professional/
 module.exports = router;
